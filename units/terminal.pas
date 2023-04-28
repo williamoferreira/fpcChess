f1b765428f9c16dc96710cd03efd38d6ad8e0073
William.Ferreira <william.ferreira@soc.com.br>
2023-04-28 18:11:11 -0300

    Metodo DrawBoard para classe TTerminal

--- units/terminal.pas
+++ units/terminal.pas
@@ -1,18 +1,50 @@
 unit terminal;
-//Unit to render console on terminal
+
 interface
 
 uses
-    piece, board, tile;
+    board;
 
 type
 
-    
+    TTerminal = class(Tobject)
+    private
+        PBoard : TBoard;
+        
+    protected
+        
+    public
+        constructor Create(const ABoard : TBoard); override;
+        procedure DrawBoard;
+
+    published
+        
+    end;
     
 
 implementation
 
 uses 
-    crt;
+    crt, Tile, Piece, exceptions;
+
+constructor TTerminal.Create(const ABoard : TBoard);
+begin
+    if ABoard = nil then
+        raise Exception.Create('Não foi informado um board válido');
+
+    Self.PBoard := ABoard;
+end;
+
+procedure TTerminal.DrawBoard;
+var
+    column, line: shortint;
+begin
+    for line := 1 to 8 do
+        for column := 1 to 8 do
+            begin
+                GotoXY(column + 4, line + 4);
+                PBoard.Board[column, line].TextOut();
+            end;
+end;
 
 end.
